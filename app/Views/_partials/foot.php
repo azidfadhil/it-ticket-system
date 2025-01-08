e01ca9f45a59014c6a9059ee370be3e8070f4439
Zeddd. <mail.azidfadhil@gmail.com>
2025-01-08 23:50:03 +0700

    add plugins dataTable

--- app/Views/_partials/foot.php
+++ app/Views/_partials/foot.php
@@ -14,7 +14,20 @@
 <!-- ChartJS -->
 <script src="<?= base_url('plugins/chart.js/Chart.min.js') ?>"></script>
 <!-- SweetAlert2 -->
-<script src="../../plugins/sweetalert2/sweetalert2.min.js"></script>
+<script src="<?= base_url('plugins/sweetalert2/sweetalert2.min.js') ?>"></script>
+<!-- DataTables  & Plugins -->
+<script src="<?= base_url('plugins/datatables/jquery.dataTables.min.js') ?>"></script>
+<script src="<?= base_url('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') ?>"></script>
+<script src="<?= base_url('plugins/datatables-responsive/js/dataTables.responsive.min.js') ?>"></script>
+<script src="<?= base_url('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') ?>"></script>
+<script src="<?= base_url('plugins/datatables-buttons/js/dataTables.buttons.min.js') ?>"></script>
+<script src="<?= base_url('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') ?>"></script>
+<script src="<?= base_url('plugins/jszip/jszip.min.js') ?>"></script>
+<script src="<?= base_url('plugins/pdfmake/pdfmake.min.js') ?>"></script>
+<script src="<?= base_url('plugins/pdfmake/vfs_fonts.js') ?>"></script>
+<script src="<?= base_url('plugins/datatables-buttons/js/buttons.html5.min.js') ?>"></script>
+<script src="<?= base_url('plugins/datatables-buttons/js/buttons.print.min.js') ?>"></script>
+<script src="<?= base_url('plugins/datatables-buttons/js/buttons.colVis.min.js') ?>"></script>
 <!-- AdminLTE App -->
 <script src="<?= base_url('dist/js/adminlte.min.js') ?>"></script>
 <!-- Is Active Script -->
--- app/Views/_partials/head.php
+++ app/Views/_partials/head.php
@@ -13,7 +13,11 @@
   <!-- Select2 -->
   <link rel="stylesheet" href="<?= base_url('plugins/select2/css/select2.min.css') ?>">
   <!-- SweetAlert2 -->
-  <link rel="stylesheet" href="../../plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
+  <link rel="stylesheet" href="<?= base_url('plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css') ?>">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="<?= base_url('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('plugins/datatables-responsive/css/responsive.bootstrap4.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') ?>">
   <!-- Theme style -->
   <link rel="stylesheet" href="<?= base_url('dist/css/adminlte.css') ?>">
 </head>
